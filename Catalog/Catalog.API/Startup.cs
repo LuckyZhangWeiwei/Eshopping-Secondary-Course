b4e2c6625a45b3e075145d66a174d92d2ce95086
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-06-29 17:35:23 +0800

    Add project files.

---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace Catalog.API;
+
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<ICatalogContext, CatalogContext>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IBrandRepository, ProductRepository>();
+        services.AddScoped<ITypesRepository, ProductRepository>();
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            //endpoints.MapHealthChecks(
+            //    "/health",
+            //    new HealthCheckOptions()
+            //    {
+            //        Predicate = _ => true,
+            //        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            //    }
+            //);
+        });
+    }
+}
