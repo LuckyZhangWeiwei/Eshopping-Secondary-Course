22502cf6198451ca7faab8d6c2c1a90e56afaa91
LuckyZhangWeiwei <zhangzhang_10@hotmail>
2024-07-01 11:13:52 +0800

    completed catalog handler

--- Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
+++ Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Responses;
 using Catalog.Core.Entities;
+using Catalog.Core.Specs;
 
 namespace Catalog.Application.Mappers;
 
@@ -9,5 +11,9 @@ public class ProductMappingProfile : Profile
     public ProductMappingProfile()
     {
         CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+        CreateMap<Product, CreateProductCommand>().ReverseMap();
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+        CreateMap<ProductType, TypesResponse>().ReverseMap();
+        CreateMap<Pagination<Product>, Pagination<ProductResponse>>().ReverseMap();
     }
 }
