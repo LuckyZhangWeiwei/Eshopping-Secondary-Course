62205a8941bd6784e8b2eda70450b83f312ab0ff
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-07-01 20:07:10 +0800

    completed 2-24

--- Catalog/Catalog.API/Startup.cs
+++ Catalog/Catalog.API/Startup.cs
@@ -1,8 +1,15 @@
 ﻿namespace Catalog.API;
 
+using System.Reflection;
+using Catalog.Application.Handlers;
 using Catalog.Core.Repositories;
 using Catalog.Infrastructure.Data;
 using Catalog.Infrastructure.Repositories;
+using HealthChecks.UI.Client;
+using MediatR;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 
 public class Startup
 {
@@ -15,6 +22,23 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddApiVersioning();
+
+        services
+            .AddHealthChecks()
+            .AddMongoDb(
+                Configuration["DatabaseSettings:ConnectionString"],
+                "Catalog  Mongo Db Health Check",
+                HealthStatus.Degraded
+            );
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+        });
+
+        services.AddAutoMapper(typeof(Startup));
+        services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+
         services.AddScoped<ICatalogContext, CatalogContext>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IBrandRepository, ProductRepository>();
@@ -28,22 +52,27 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
         }
+
+        app.UseHttpsRedirection();
         app.UseRouting();
+        // app.UseCors("CorsPolicy");
+        app.UseAuthentication();
         app.UseStaticFiles();
         app.UseAuthorization();
-
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
-            //endpoints.MapHealthChecks(
-            //    "/health",
-            //    new HealthCheckOptions()
-            //    {
-            //        Predicate = _ => true,
-            //        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            //    }
-            //);
+            endpoints.MapHealthChecks(
+                "/health",
+                new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                }
+            );
         });
     }
 }
