bb877d6ddcfcf0a9b0043d6b2fb78265e312ba57
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-07-13 17:18:18 +0800

    completed 4-11

---
+++ Services/Discount/Discount.Application/Handlers/GetDiscountQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.Application.Handlers;
+
+public class GetDiscountQueryHandler : IRequestHandler<GetDiscountQuery, CouponModel>
+{
+    private readonly IDiscountRepository _discountRespoitory;
+    private readonly IMapper _mapper;
+
+    public GetDiscountQueryHandler(IDiscountRepository discountRepository, IMapper mapper)
+    {
+        _discountRespoitory = discountRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<CouponModel> Handle(
+        GetDiscountQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        var coupon = await _discountRespoitory.GetDiscount(request.ProductName);
+
+        if (coupon == null)
+        {
+            throw new RpcException(
+                new Status(
+                    StatusCode.NotFound,
+                    $"Discount with product name = {request.ProductName} not found"
+                )
+            );
+        }
+
+        return _mapper.Map<CouponModel>(coupon);
+    }
+}
