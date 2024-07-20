a5181d88a17f43a8be13b3f9416e6b983123f9e4
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-07-20 10:59:22 +0800

    completed 6-11

---
+++ Services/Order/Ordering.Application/Mappers/OrderMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ording.Core.Entities;
+
+namespace Ordering.Application.Mappers
+{
+    public class OrderMapperProfile : Profile
+    {
+        public OrderMapperProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
