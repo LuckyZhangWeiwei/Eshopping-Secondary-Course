7e3e42e9763f381f37d44af6ed74108e493c7ac5
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-07-20 09:27:08 +0800

    completed 6-03

---
+++ Services/Order/Ording.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ording.Core.Entities;
+
+namespace Ording.Core.Repositories;
+
+public interface IOrderRepository : IAsyncRepository<Order>
+{
+    Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+}
