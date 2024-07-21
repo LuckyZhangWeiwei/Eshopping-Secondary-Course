88162fda1681c7e5e20b09557380304af7142238
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-07-21 10:14:03 +0800

    completed 6-19

---
+++ Services/Order/Ordering.Application/Extensions/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+
+namespace Ordering.Application.Extensions;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        var assemblies = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(assemblies);
+
+        services.AddMediatR(assemblies);
+
+        services.AddValidatorsFromAssembly(assemblies);
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+
+        return services;
+    }
+}
